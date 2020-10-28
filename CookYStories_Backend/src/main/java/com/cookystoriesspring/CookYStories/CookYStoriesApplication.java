51c268a542f2775a679349bc3824a3ac9f0e0137
Prathma Rastogi <prathma@Prathmas-MBP.fios-router.home>
2020-10-28 01:16:51 -0400

    boilerplate frontend and backend added

---
+++ CookYStories_Backend/src/main/java/com/cookystoriesspring/CookYStories/CookYStoriesApplication.java
@@ -0,0 +1,13 @@
+package com.cookystoriesspring.CookYStories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookYStoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookYStoriesApplication.class, args);
+	}
+
+}
---
+++ CookYStories_Backend/src/test/java/com/cookystoriesspring/CookYStories/CookYStoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cookystoriesspring.CookYStories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookYStoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
