19edc7265f95b66e975ee16e4632f18c74ee7cf5
Prathma Rastogi <prathma@Prathmas-MacBook-Pro.local>
2020-12-07 20:19:18 -0800

    AWS Path and local path

--- CookYStories_Backend/src/main/java/com/cookystoriesspring/CookYStories/Utils/SeleniumConfiguration.java
+++ CookYStories_Backend/src/main/java/com/cookystoriesspring/CookYStories/Utils/SeleniumConfiguration.java
@@ -6,12 +6,16 @@ import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
 
+
 @Configuration
 public class SeleniumConfiguration {
 
+    public static final String AWS_CHROMEDRIVER_PATH = "/home/ec2-user/PROJECT/chromedriver";
+    public static final String DEV_CHROMEDRIVER_PATH = "/Users/prathma/Documents/OOD_PROJECT/chromedriver";
+
     @PostConstruct
     public void postConstruct() {
-        System.setProperty("webdriver.chrome.driver", "/Users/maitreyasatavalekar/Downloads/chromedriver");
+        System.setProperty("webdriver.chrome.driver", AWS_CHROMEDRIVER_PATH);
     }
 
     @Bean
