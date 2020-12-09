fdd192334ccc01528804be738446448b0ea95156
pratik <prbhatia@syr.edu>
2020-12-10 00:17:45 +0530

    channels and near me

---
+++ CookYStories_Frontend/src/userFeed/components/Channels.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import '../css/SidebarRow.css'
+
+export default class Channels extends Component {
+    render() {
+        return (
+            <div className='channels'>
+                {/* {this.props.statusImage} */}
+                <h6>{this.props.title}</h6>
+            </div>
+        )
+    }
+}
