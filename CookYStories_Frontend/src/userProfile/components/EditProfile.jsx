fabf3fabbdae11754dc3a5151d85f0d3133b38ae
Prathma Rastogi <prathma@Prathmas-MacBook-Pro.local>
2020-12-10 18:28:50 -0800

    profile edit done

--- CookYStories_Frontend/src/userProfile/components/EditProfile.jsx
+++ CookYStories_Frontend/src/userProfile/components/EditProfile.jsx
@@ -4,6 +4,8 @@ import Button from '@material-ui/core/Button';
 import React, { Component,  useCallback } from 'react'
 import '../css/EditProfile.css'
 import SaveIcon from '@material-ui/icons/Save';
+import {UPDATE_USER_INFO} from '../../backend/UserProfileApis'
+import { Mutation } from 'react-apollo'
 
 class EditProfile extends Component {
 
@@ -11,142 +13,188 @@ class EditProfile extends Component {
     super(props)
     this.state = {
       user: {
-        first_name: props.firstName,
-        last_name: props.lastName,
-        user_name: props.username,
-        city: props.city,
-        country: props.country,
-        bio: props.bio,
-        profileImageUrl: props.profileImageUrl
-      }
+        first_name: props.user.first_name,
+        last_name: props.user.last_name,
+        city: props.user.city,
+        country: props.user.country,
+        bio: props.user.bio,
+      },
+      profileImageUrl: props.profileImageUrl,
+      user_name: props.user_name,
+      // openModal:false
     };
   }
 
-  componentWillReceiveProps(props) {
-
+  handleChange = event => {
     this.setState({
       user: {
-        first_name: props.firstName,
-        last_name: props.lastName,
-        user_name: props.username,
-        city: props.city,
-        country: props.country,
-        bio: props.bio,
-        profileImageUrl: props.profileImageUrl
+        [event.target.name]: event.target.value
       }
-    })
+    });
   }
 
-  handleChange = event => {
-    this.props.onUserChange(event)
+  onDataUpdate = (param) => {
+    this.props.onUserChange(param)
   }
 
-  
  render() {
   let showHideClassName = this.props.show ? " modal d-block" : 'modal d-none';
 
+  // if (this.props.show) {
+  //   this.setState({
+  //     openModal:true
+  //   })
+  // } else {
+  //   this.setState({
+  //     openModal:false
+  //   })
+  // }
   return (
-    <div className={showHideClassName}>
-        <div className="modal-container">
-            <form className="form-group">
-                <div className='EditImage'>
-                    <img src={this.state.user.profileImageUrl} alt="img"/>
-                </div>
-                <ul>
-                    <div className="form-group">
-                        <label >First name:</label>
-                        <input
-                          type="text"
-                          value={this.state.user.first_name}
-                          name="first_name"
-                          onChange={e => this.handleChange(e)}
-                          className="form-control"
-                          placeholder="Rushang"
-                          style={{ display: 'flex' }} />             
-                    </div>
-                    <div className="form-group">
-                        <label>Last name:</label>
-                        <input
-                          type="text"
-                          value={this.state.user.last_name}
-                          name="last_name"
-                          onChange={e => this.handleChange(e)}
-                          className="form-control"
-                          placeholder="Shah"
-                          style={{display: 'flex'}}
-                        />
-                    </div>
-                    {/* <div className="form-group">
-                          <label>User name:</label>
+    
+
+<Mutation mutation={UPDATE_USER_INFO} variables={
+        {
+            username: this.state.user_name, 
+            firstName: this.state.user.first_name, 
+            lastName: this.state.user.last_name, 
+            city: this.state.user.city, 
+            country: this.state.user.country, 
+            bio: this.state.user.bio,
+            profileImageUrl: this.state.profileImageUrl
+        }}> 
+{
+  (onUpdateClick, {loading, error, data}) => {
+
+        if (loading) {
+                  
+        }
+
+        if (error) {
+
+        } 
+        if (data && this.props.show) {
+          console.log(data)
+          const updatedUser = data.updateUser
+          this.onDataUpdate(updatedUser)
+        }
+        return (
+          <div className={showHideClassName}>
+          <div className="modal-container">
+              <form className="form-group">
+                  <div className='EditImage'>
+                      <img src={this.state.profileImageUrl} alt="img"/>
+                  </div>
+                  <ul>
+                      <div className="form-group">
+                          <label >First name:</label>
                           <input
                             type="text"
-                            value={this.state.user_name}
-                            name="user_name"
+                            value={this.state.user.first_name}
+                            name="first_name"
                             onChange={e => this.handleChange(e)}
                             className="form-control"
-                            placeholder="rushang2413"
-                            style={{display: 'flex'}}
-                          />
-                    </div> */}
-                    <div className="form-group">
-                        <label>City:</label>
-                        <input
-                          type="text"
-                          value={this.state.user.city}
-                          name="city"
-                          onChange={e => this.handleChange(e)}
-                          className="form-control"
-                          placeholder="Syracuse"
-                          style={{display: 'flex'}}
-                        />
-                        <label style={{marginLeft:'10px'}}>Country:</label>
-                        <input
-                          type="text"
-                          value={this.state.user.country}
-                          name="country"
-                          onChange={e => this.handleChange(e)}
-                          className="form-control"
-                          placeholder="USA"
-                          style={{marginLeft: '5px'}}
-                        />
-                    </div>
-                    <div className="form-group">
-                          <label>Bio:</label>
-                          <textarea
+                            placeholder="Rushang"
+                            style={{ display: 'flex' }} />             
+                      </div>
+                      <div className="form-group">
+                          <label>Last name:</label>
+                          <input
                             type="text"
-                            value={this.state.user.bio}
-                            name="bio"
+                            value={this.state.user.last_name}
+                            name="last_name"
                             onChange={e => this.handleChange(e)}
                             className="form-control"
-                            placeholder="Hello Everyone!!!!!!!!!"
+                            placeholder="Shah"
                             style={{display: 'flex'}}
                           />
-                    </div>  
-                  </ul>  
-              </form>
-              <form>
-                <div className="button">
-                  <Button
-                    variant="contained"
-                    color="primary"                  
-                    startIcon={<SaveIcon />}
-                    onClick={this.props.handleUpdate} >
-                    Update
-                  </Button>
-                </div>
-                <div className="button">
-                  <Button
-                    variant="contained"
-                    color="secondary"
-                    className='modal-close'
-                    onClick={this.props.handleClose}
-                    startIcon={<DeleteIcon />}>
-                    Cancel
-                  </Button>
-                </div>
-              </form>        
-          </div>
-    </div>
+                      </div>
+                      {/* <div className="form-group">
+                            <label>User name:</label>
+                            <input
+                              type="text"
+                              value={this.state.user_name}
+                              name="user_name"
+                              onChange={e => this.handleChange(e)}
+                              className="form-control"
+                              placeholder="rushang2413"
+                              style={{display: 'flex'}}
+                            />
+                      </div> */}
+                      <div className="form-group">
+                          <label>City:</label>
+                          <input
+                            type="text"
+                            value={this.state.user.city}
+                            name="city"
+                            onChange={e => this.handleChange(e)}
+                            className="form-control"
+                            placeholder="Syracuse"
+                            style={{display: 'flex'}}
+                          />
+                          <label style={{marginLeft:'10px'}}>Country:</label>
+                          <input
+                            type="text"
+                            value={this.state.user.country}
+                            name="country"
+                            onChange={e => this.handleChange(e)}
+                            className="form-control"
+                            placeholder="USA"
+                            style={{marginLeft: '5px'}}
+                          />
+                      </div>
+                      <div className="form-group">
+                            <label>Bio:</label>
+                            <textarea
+                              type="text"
+                              value={this.state.user.bio}
+                              name="bio"
+                              onChange={e => this.handleChange(e)}
+                              className="form-control"
+                              placeholder="Hello Everyone!!!!!!!!!"
+                              style={{display: 'flex'}}
+                            />
+                      </div>  
+                    </ul>  
+                </form>
+                <form>
+                  <div className="button">
+                    <Button
+                      variant="contained"
+                      color="primary"                  
+                      startIcon={<SaveIcon />}
+                      onClick={e => {
+                        onUpdateClick({variables: {
+                          username: this.state.user_name, 
+                          firstName: this.state.user.first_name, 
+                          lastName: this.state.user.last_name, 
+                          city: this.state.user.city, 
+                          country: this.state.user.country, 
+                          bio: this.state.user.bio,
+                          profileImageUrl: this.state.profileImageUrl
+                          }
+                        });
+                        }} >
+                      Update
+                    </Button>
+                  </div>
+                  <div className="button">
+                    <Button
+                      variant="contained"
+                      color="secondary"
+                      className='modal-close'
+                      onClick={this.props.handleClose}
+                      startIcon={<DeleteIcon />}>
+                      Cancel
+                    </Button>
+                  </div>
+                </form>        
+            </div>
+      </div>
+          
+        )
+  }
+}
+</Mutation>   
   )
   }
 }
--- CookYStories_Frontend/src/userProfile/pages/ProfileHeader.jsx
+++ CookYStories_Frontend/src/userProfile/pages/ProfileHeader.jsx
@@ -9,8 +9,8 @@ import AuthenticationService from '../../backend/AuthenticationService.js'
 import ErrorIcon from '@material-ui/icons/Error';
 import TrendingUpIcon from '@material-ui/icons/TrendingUp';
 // import CheckIcon from '@material-ui/icons/Check';
-import {UPDATE_USER_INFO} from '../../backend/UserProfileApis'
-import { Mutation } from 'react-apollo'
+// import {UPDATE_USER_INFO} from '../../backend/UserProfileApis'
+// import { Mutation } from 'react-apollo'
 
 
 export default class ProfileHeader extends Component {
@@ -26,36 +26,31 @@ export default class ProfileHeader extends Component {
       userData: {
         first_name: props.firstName,
         last_name: props.lastName,
-        user_name: props.username,
         city: props.city,
         country: props.country,
-        bio: props.bio,
-        profileImageUrl: props.profileImageUrl
+        bio: props.bio
       },
+      profileImageUrl: props.profileImageUrl,
+      user_name: props.username,
       isFollowed: props.isFollowed
     };
   }
 
-  componentWillReceiveProps(props) {
-    this.setState(
-      {
-        userData: {
-          first_name: props.firstName,
-          last_name: props.lastName,
-          user_name: props.username,
-          city: props.city,
-          country: props.country,
-          bio: props.bio,
-          profileImageUrl: props.profileImageUrl
-        },
-        isFollowed: props.isFollowed
-      }
-    )
-  }
 
-  onUserChange = (e) => {
+  onUserChange = (param) => {
+    
     this.setState({
-      [e.target.name]: e.target.value
+      userData: {
+        first_name: param.firstName,
+        last_name: param.lastName,
+        city: param.city,
+        country: param.country,
+        bio: param.bioDescription
+      },
+      profileImageUrl: param.profileImageUrl,
+      user_name: param.username,
+      modal: false
+
     });
   }
 
@@ -79,79 +74,46 @@ export default class ProfileHeader extends Component {
   render() {
     let imageUrl
 
-    if (this.state.userData.profileImageUrl != null && this.state.userData.profileImageUrl != "") {
-      imageUrl = this.state.userData.profileImageUrl
+    if (this.state.profileImageUrl != null && this.state.profileImageUrl != "") {
+      imageUrl = this.state.profileImageUrl
     } else {
       imageUrl = {chefAvatar}
     }
 
     return (      
-       
-      <Mutation mutation={UPDATE_USER_INFO}> 
-      {
-        (onUpdateClick, {loading, error, data}) => {
-            const onUpdateFunc = () => onUpdateClick(
-              {
-                variables:
-                {
-                  username: this.state.userData.user_name, 
-                  firstName: this.state.userData.first_name, 
-                  lastName: this.state.userData.last_name, 
-                  city: this.state.userData.city, 
-                  country: this.state.userData.country, 
-                  bio: this.state.userData.bio
-                }
-              })
-
-              if (loading) {
-                        
-              }
-
-              if (error) {
-
-              } 
-              if (data) {
-                console.log(data)
-                // this.setState({
-
-                // })
-              }
-              return (
-
-                <div className='userProfile'>
-                    <div className='profileImage'>
-                        <img src={imageUrl} alt="Img"/>
-                    </div>
-                    <div className='profileBio'>
-                      <h4>{this.state.userData.first_name} {this.state.userData.last_name}</h4>
-                      <div className='profileData'>
-                          <h5>{this.state.userData.user_name}</h5>
-                          <h5>{this.state.userData.bio}</h5>
-                          <h5>{this.state.userData.city}, {this.state.userData.country}</h5>
+                  
+      <div className='userProfile'>
+                      <div className='profileImage'>
+                          <img src={imageUrl} alt="Img"/>
                       </div>
-                      {this.state.userData.user_name !== AuthenticationService.getLoggedInUser() && 
-                          <>
-                          {this.state.isFollowed && 
-                            <Button variant="contained" style={{ color: "green" }} endIcon={<TrendingUpIcon />} style={{ marginLeft: '900px' }}>Unfollow</Button>
-                          }
-                          {!this.state.isFollowed && 
-                            <Button variant="contained" color="primary" endIcon={<TrendingUpIcon />} style={{ marginLeft: '900px' }}>Follow</Button>
-                          }
-                          <ErrorIcon style={{ fill: "red" }}/>
-                                  
-                          </>
-                      }
-                      { this.state.userData.user_name == AuthenticationService.getLoggedInUser() &&
-                          <Button className="editProfile" onClick={e => this.modalOpen(e)} variant="contained"
-                          startIcon={<CreateIcon />}>Edit Profile</Button>
-                      }
-                      <EditProfile show={this.state.modal} onUserChange={(e) => this.onUserChange(e)} handleUpdate={onUpdateFunc}  handleClose={e => this.modalClose(e)} user={this.state.userData}/>         
-                    </div>
+                      <div className='profileBio'>
+                        <h4>{this.state.userData.first_name} {this.state.userData.last_name}</h4>
+                        <div className='profileData'>
+                            <h5>{this.state.user_name}</h5>
+                            <h5>{this.state.userData.bio}</h5>
+                            <h5>{this.state.userData.city}, {this.state.userData.country}</h5>
+                        </div>
+                        {this.state.user_name !== AuthenticationService.getLoggedInUser() && 
+                            <>
+                            {this.state.isFollowed && 
+                              <Button variant="contained" style={{ color: "green" }} endIcon={<TrendingUpIcon />} style={{ marginLeft: '900px' }}>Unfollow</Button>
+                            }
+                            {!this.state.isFollowed && 
+                              <Button variant="contained" color="primary" endIcon={<TrendingUpIcon />} style={{ marginLeft: '900px' }}>Follow</Button>
+                            }
+                            <ErrorIcon style={{ fill: "red" }}/>
+                                    
+                            </>
+                        }
+                        { this.state.user_name === AuthenticationService.getLoggedInUser() &&
+                            <Button className="editProfile" onClick={e => this.modalOpen(e)} variant="contained"
+                            startIcon={<CreateIcon />}>Edit Profile</Button>
+                        }
+                        <EditProfile show={this.state.modal} onUserChange={(e) => this.onUserChange(e)} handleUpdate={(e) => this.handleUpdate(e)}  handleClose={e => this.modalClose(e)} user={this.state.userData} profileImageUrl={imageUrl} user_name={this.state.user_name}/>         
+                        
+                      </div>
                 </div> 
-              )
-        }
-      }
-      </Mutation>         
+            
     )
   }
 }
