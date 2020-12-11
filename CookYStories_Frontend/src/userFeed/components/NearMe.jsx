6a29902e31b79bf486bb07d4452c69772b530fd6
pratik <prbhatia@syr.edu>
2020-12-11 09:08:13 +0530

    near me remaining

--- CookYStories_Frontend/src/userFeed/components/NearMe.jsx
+++ CookYStories_Frontend/src/userFeed/components/NearMe.jsx
@@ -1,29 +1,57 @@
-import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+// import React, { Component } from 'react';
+// import { Map, GoogleApiWrapper } from 'google-maps-react';
 
-const mapStyles = {
-  width: '100%',
-  height: '100%'
-};
+// const mapStyles = {
+//   width: '100%',
+//   height: '100%'
+// };
 
-export class MapContainer extends Component {
-  render() {
+// export class MapContainer extends Component {
+//     const showHideClassName = show ? " modal d-block" : 'modal d-none';
+//   render() {
+//     return (
+//         <Map
+//         google={this.props.google}
+//         zoom={14}
+//         style={mapStyles}
+//         initialCenter={
+//           {
+//             lat: 19.075983,
+//             lng: 72.877655
+//           }
+//         }
+//       />
+//     );
+//   }
+// }
+
+// export default GoogleApiWrapper({
+//   apiKey: 'AIzaSyBEFspxaaFdg-dhYys7Bg9y2MaH6ixh83E'
+// })(MapContainer);  
+import ArrowBackIosOutlinedIcon from '@material-ui/icons/ArrowBackIosOutlined';
+import Button from '@material-ui/core/Button';
+import React from 'react'
+
+export const NearMe = ({ handleClose, show, children }) => {
+    const showHideClassName = show ? "modal d-block" : 'modal d-none';
     return (
-      <Map
-        google={this.props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={
-          {
-            lat: 19.075983,
-            lng: 72.877655
-          }
-        }
-      />
-    );
+      <div className={showHideClassName}>
+        <div className="modal-container">
+          {/* {children} */}
+          <div className="button">
+          <Button
+            variant="contained"
+            color="secondary"
+            className='modal-close'
+            onClick={handleClose}
+            startIcon={ArrowBackIosOutlinedIcon}
+            >
+              Back
+        </Button>
+        </div>
+        </div>
+        
+      </div>
+    )
   }
-}
-
-export default GoogleApiWrapper({
-  apiKey: 'AIzaSyBEFspxaaFdg-dhYys7Bg9y2MaH6ixh83E'
-})(MapContainer);
+  export default NearMe;
--- CookYStories_Frontend/src/userFeed/components/Sidebar.jsx
+++ CookYStories_Frontend/src/userFeed/components/Sidebar.jsx
@@ -1,40 +1,125 @@
 import React, {Component} from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import {useState} from 'react';
+import Modal from 'react-modal';
+import GoogleMapReact from 'google-map-react';
 import '../css/Sidebar.css';
 import NearMeIcon from '@material-ui/icons/NearMe';
 import RestaurantIcon from '@material-ui/icons/Restaurant';
-import MenuBookIcon from '@material-ui/icons/MenuBook';
-import VideocamIcon from '@material-ui/icons/Videocam';
 import KitchenIcon from '@material-ui/icons/Kitchen';
 import FeaturedPlayListIcon from '@material-ui/icons/FeaturedPlayList';
 import SidebarRow from './SidebarRow'
 import { Grid } from '@material-ui/core';
 import Channels from './Channels'
 import NearMe from './NearMe'
+import Button from '@material-ui/core/Button';
+import '../css/SidebarRow.css'
+
+
+// const [modalIsOpen,setIsOpen] = React.useState(false);
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 export default class Sidebar extends Component {
+
+  constructor(props) {
+    super(props)
+    // this.handleChange = this.handleChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+    this.modalOpen = this.modalOpen.bind(this);
+    this.modalClose = this.modalClose.bind(this);
+
+    this.state = {
+      modal: false,
+    };
+  }
+
+  static defaultProps = {
+    center: {
+      lat: 19.075983,
+      lng: 72.877655
+    },
+    zoom: 15
+  };
+
+
+  // handleChange(e) {
+  //   // const target = e.target;
+  //   // const name = target.name;
+  //   // const value = target.value;
+
+  //   this.setState({
+  //     [e.target.name]: e.target.value
+  //   });
+  // }
+
+  // handleSubmit(e) {
+  //   // const { first_name, last_name, user_name, city, country, bio } = this.state;
+  //   this.setState({ name: this.state.modalInputName });
+  //   this.modalClose();
+  // }
+
+  modalOpen() {
+    this.setState({ modal: true });
+  }
+
+  modalClose() {
+    this.setState({
+      modal: false
+    });
+  }
+
 
     render() {
         return (
           <div className="sidebar">
            <Grid item xs={12} spacing={3}>
 
-              <SidebarRow statusImage={<NearMeIcon />} title="NEAR ME" /> {onclick = <NearMe />}
-              <hr />
-              <SidebarRow statusImage={<RestaurantIcon />} title="RESTAURANTS" />
-              <hr />
-              <SidebarRow statusImage={<FeaturedPlayListIcon />} title="CHANNELS" /> 
+              <div className='sidebarRow' onClick={e => this.modalOpen(e)}>
+                <NearMeIcon />
+                <h6>NEAR ME</h6>
+                <Modal show={this.state.modal} handleClose={e => this.modalClose(e)}>
+                <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: /* YOUR KEY HERE */ }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text="My Marker"
+          />
+        </GoogleMapReact>
+      </div>
+                </Modal>
+                
+                <hr />
+              </div>
+
+              <div className='sidebarRow'>
+                <RestaurantIcon />
+                <h6>RESTAURANTS</h6>
+                <hr/>
+              </div>
+
+              <div className='sidebarRow'>
+                <FeaturedPlayListIcon />
+                <h6>CHANNELS</h6>
+              </div>
+
               <Channels statusImage={<KitchenIcon />} title="Gordon Ramsay" /> 
               <Channels statusImage={<KitchenIcon />} title="Chef Ranveer" /> 
               <Channels statusImage={<KitchenIcon />} title="Your Food Lab" /> 
               <Channels statusImage={<KitchenIcon />} title="Kabita's Kitchen" /> 
               <Channels statusImage={<KitchenIcon />} title="Hebbar's Kitchen" /> 
               
-              {/* <SidebarRow statusImage={<VideocamIcon />} title="Live Cooking" /> */}
            </Grid>
            
           </div>
         );
+    }
+
+    linkClicked = () => {
+      this.props.history.push('/signup');
     }
 }
 
